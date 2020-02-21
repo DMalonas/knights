fb61afdd00dd5480da4cc0b7ab3e14c0d8b2ca70
Dimitri <dimitri@lindar.com>
2020-02-21 10:05:09 +0000

    XML configuration for dependency injection used

---
+++ src/main/java/KnightMain.java
@@ -0,0 +1,12 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class KnightMain {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext(
+                        "Knight.xml");
+        Knight knight = (Knight) context.getBean(Knight.class);
+        knight.embarkOnQuest();
+        context.close();
+    }
+}
---
+++ src/main/java/RescueDamselQuest.java
@@ -0,0 +1,5 @@
+public class RescueDamselQuest implements Quest {
+    public void embark() {
+        System.out.println("Embarking on a quest to rescue the damsel.");
+    }
+}
